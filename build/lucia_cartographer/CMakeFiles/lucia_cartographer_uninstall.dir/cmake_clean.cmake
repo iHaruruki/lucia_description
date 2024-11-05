1792fab56b2f94d2cc22664617137235e95e468c
Haruki Isono <haruki.isono861@gmail.com>
2024-11-05 14:48:52 +0900

    add: add .gitignore file

---
+++ build/lucia_cartographer/CMakeFiles/lucia_cartographer_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lucia_cartographer_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/lucia_cartographer_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
