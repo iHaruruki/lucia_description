d70ef40f48375310aa1a42d66c82ee0a4e8efabf
Haruki Isono <haruki.isono861@gmail.com>
2024-11-22 16:39:55 +0900

    fix: lucia_cartographer config/noimu_lds_2d.lua
    https://hackmd.io/@kzs/ByElvVd_p を参考にした

--- lucia_bringup/config/noimu_lds_2d.lua
+++ lucia_bringup/config/noimu_lds_2d.lua
@@ -5,12 +5,12 @@ options = {
   map_builder = MAP_BUILDER,
   trajectory_builder = TRAJECTORY_BUILDER,
   map_frame = "map",
-  tracking_frame = "base_link", --"imu_link",
-  published_frame = "base_link", --"odom",
+  tracking_frame = "laser_frame",
+  published_frame = "laser_frame",
   odom_frame = "odom",
-  provide_odom_frame = true, --false,
-  publish_frame_projected_to_2d = true,
-  use_odometry = false, --true,
+  provide_odom_frame = false,
+  publish_frame_projected_to_2d = false,
+  use_odometry = false,
   use_nav_sat = false,
   use_landmarks = false,
   num_laser_scans = 1,
@@ -18,7 +18,7 @@ options = {
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
-  submap_publish_period_sec = 0.3,
+  submap_publish_period_sec = 0.05,
   pose_publish_period_sec = 5e-3,
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
@@ -30,21 +30,26 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.min_range = 1.0 --0.12
-TRAJECTORY_BUILDER_2D.max_range = 8.0 --3.5
-TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
+TRAJECTORY_BUILDER_2D.min_range = 0.3
+TRAJECTORY_BUILDER_2D.max_range = 12.0
+TRAJECTORY_BUILDER_2D.missing_data_ray_length = 5.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
-TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true 
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
+TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
 
 POSE_GRAPH.constraint_builder.min_score = 0.65
 POSE_GRAPH.constraint_builder.global_localization_min_score = 0.7
 
---POSE_GRAPH.optimize_every_n_nodes = 0
+POSE_GRAPH.optimization_problem.local_slam_pose_translation_weight = 1e5
+POSE_GRAPH.optimization_problem.local_slam_pose_rotation_weight = 1e5
+POSE_GRAPH.optimization_problem.odometry_translation_weight = 1e5
+POSE_GRAPH.optimization_problem.odometry_rotation_weight = 1e5
+POSE_GRAPH.optimization_problem.huber_scale = 1e3
 
-TRAJECTORY_BUILDER.pure_localization_trimmer = {
-    max_submaps_to_keep = 3,
-}
-POSE_GRAPH.optimize_every_n_nodes = 20
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.occupied_space_weight = 10
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 40
+
+TRAJECTORY_BUILDER_2D.submaps.num_range_data = 120
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.2)
 
 return options
