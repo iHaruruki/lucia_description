9ab0f7b584452c9bb95f03eff34f00bc9dd954c6
Haruki <haruki.isono861@gmail.com>
2025-03-28 10:15:44 +0900

    fix

--- build/lucia_cartographer/CMakeFiles/lucia_cartographer_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lucia_cartographer_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lucia_cartographer_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/lucia_slam/CMakeFiles/lucia_slam_uninstall.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for custom commands dependencies management for lucia_slam_uninstall.
