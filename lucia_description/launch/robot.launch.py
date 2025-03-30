504088283bb056b00609335b38d43ae8d2e755c4
Haruki <haruki.isono861@gmail.com>
2025-03-30 20:54:19 +0900

    add: launch file

---
+++ lucia_description/launch/robot.launch.py
@@ -0,0 +1,27 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
+        ),
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_pushlisher',
+            output='screen',
+            parameters=[{'robot_description': open('/urdf/my_robot.urdf').read()}]
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
