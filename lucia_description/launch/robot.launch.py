602ad32a375afa034868323c954fd78c928a8ed5
Haruki <haruki.isono861@gmail.com>
2025-03-31 00:46:11 +0900

    add: oint_state_publisher_gui

--- lucia_description/launch/robot.launch.py
+++ lucia_description/launch/robot.launch.py
@@ -20,9 +20,9 @@ def generate_launch_description():
 
     return LaunchDescription([
         Node(
-            package='joint_state_publisher',
-            executable='joint_state_publisher',
-            name='joint_state_publisher',
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
             output='screen',
         ),
         Node(
