db735fa6a7ff4a8ed719ce6cab8c894a9dabe2cf
HarkiIsono <haruki.isono861@gmail.com>
2025-06-08 01:41:18 +0900

    fix

--- launch/robot.launch.py
+++ launch/robot.launch.py
@@ -2,21 +2,20 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.substitutions import Command, FindExecutable, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     package_name = 'lucia_description'  # package name
     urdf_file_name = 'lucia.urdf'    # URDF file name
 
-    # パッケージの共有ディレクトリを取得
-    urdf_file_path = os.path.join(
-        get_package_share_directory(package_name),
-        'urdf',
-        urdf_file_name
-    )
-
-    # URDFの内容を取得
-    with open(urdf_file_path, 'r') as infp:
-        robot_desc = infp.read()
+    # build the URDF string by xacro
+    robot_description = Command([
+        FindExecutable(name='xacro'), ' ',
+        PathJoinSubstitution([
+        FindPackageShare('lucia_description'), 'urdf', 'lucia.urdf'
+        ])
+    ])
 
     return LaunchDescription([
         Node(
@@ -30,7 +29,10 @@ def generate_launch_description():
             executable='robot_state_publisher',
             name='robot_state_pushlisher',
             output='screen',
-            parameters=[{'robot_description': robot_desc}]
+            parameters=[
+                {'robot_description': robot_description},
+                {'publish_robot_description': True}
+            ]
         )
         #Node(
         #    package='rviz2',
